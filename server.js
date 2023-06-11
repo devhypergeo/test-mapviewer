4b981581ed3d7b04a30d79bb20af92e648d8cfb7
devhypergeo <59184125+devhypergeo@users.noreply.github.com>
2023-06-11 16:06:20 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// const cors = require("cors");
+// const corsOptions = {
+//     origin: '*',
+//     credentials: true, //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+// }
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
+// app.use(cors(corsOptions))
+app.use(express.static(__dirname + '/public'))
+app.use('/assets', express.static('/public/assets/'))
+
+app.get('/', function(req, res) {
+    res.sendFile('./index.html', { root: __dirname })
+})
+
+app.get('/three-js', function(req, res) {
+    res.sendFile('./node_modules/three/build/three.module.js', { root: __dirname })
+})
+
+app.listen(port, () => {
+    console.log('running server port 3000')
+})
